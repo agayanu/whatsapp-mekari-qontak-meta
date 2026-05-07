a484ff0e9e776ce2093b539fef95202630ed5e3e
Aga Yanu <agayanu.ay@gmail.com>
2026-05-07 14:12:29 +0700

    update regist, acc, pay

--- main.php
+++ main.php
@@ -90,51 +90,91 @@ function error_function($msg,$waError,$detail = NULL) {
 // Set request
 $category    = null;
 $phone       = null;
-$parentName  = null;
 $studentName = null;
+$school      = 'SMA Plus PGRI Cibinong';
+$schoolYear  = 'Tahun Ajaran 2026/2027';
 if (isset($_POST['category']))     { $category    = $_POST['category']; }     // Example : absence
 if (isset($_POST['phone']))        { $phone       = $_POST['phone']; }        // Example : 6281284420481
-if (isset($_POST['parent_name']))  { $parentName  = $_POST['parent_name']; }  // Example : ANDRI SIREGAR/NANA SIREGAR
 if (isset($_POST['student_name'])) { $studentName = $_POST['student_name']; } // Example : FERNANDES SIREGAR
 If (!in_array($category,['absence','payment','bill'])) {error_function('Category cannot NULL!',$waError);}
 If (!$phone)       {error_function('Phone cannot NULL!',$waError);}
-If (!$parentName)  {error_function('Parent Name cannot NULL!',$waError);}
 If (!$studentName) {error_function('Student Name cannot NULL!',$waError);}
-$parentName = str_replace('Yth.', '', $parentName);
-$parentName = str_replace('Yth ', '', $parentName);
-$parentName = str_replace('Yth. ', '', $parentName);
 
 if ($category === 'absence') {
+    $parentName    = null;
     $absenceDate   = null;
     $absenceStatus = null;
     $absenceRemark = null;
+    if (isset($_POST['parent_name']))    { $parentName  = $_POST['parent_name']; }  // Example : ANDRI SIREGAR/NANA SIREGAR
     if (isset($_POST['trans_date']))     { $absenceDate   = $_POST['trans_date']; }   // Example : 18-Okt-2025
     if (isset($_POST['absence_status'])) { $absenceStatus = $_POST['absence_status']; } // Example : SAKIT
     if (isset($_POST['absence_remark'])) { $absenceRemark = $_POST['absence_remark']; } // Example : Info dari guru kelas
+    If (!$parentName)    {error_function('Parent Name cannot NULL!',$waError);}
     If (!$absenceDate)   {error_function('Absence Date cannot NULL!',$waError);}
     If (!$absenceStatus) {error_function('Absence Status cannot NULL!',$waError);}
     If (!$absenceRemark) {error_function('Absence Remark cannot NULL!',$waError);}
+    $parentName = str_replace('Yth.', '', $parentName);
+    $parentName = str_replace('Yth ', '', $parentName);
+    $parentName = str_replace('Yth. ', '', $parentName);
 }
 if ($category === 'payment') {
+    $parentName   = null;
     $studentClass = null;
     $paymentInfo  = null;
     $paymentDate = null;
+    if (isset($_POST['parent_name']))   { $parentName   = $_POST['parent_name']; }   // Example : ANDRI SIREGAR/NANA SIREGAR
     if (isset($_POST['student_class'])) { $studentClass = $_POST['student_class']; } // Example : XI.R-3
     if (isset($_POST['message']))       { $paymentInfo  = $_POST['message']; }  // Example : SPP Nov-2025: 600,000
     if (isset($_POST['trans_date']))    { $paymentDate  = $_POST['trans_date']; }  // Example : 29-Okt-2025
+    if (!$parentName)   {error_function('Parent Name cannot NULL!',$waError);}
     If (!$studentClass) {error_function('Student Class cannot NULL!',$waError);}
     If (!$paymentInfo)  {error_function('Payment Info cannot NULL!',$waError);}
     If (!$paymentDate)  {error_function('Payment Date cannot NULL!',$waError);}
+    $parentName = str_replace('Yth.', '', $parentName);
+    $parentName = str_replace('Yth ', '', $parentName);
+    $parentName = str_replace('Yth. ', '', $parentName);
 }
 if ($category === 'bill') {
+    $parentName   = null;
     $studentClass = null;
     $billList     = null;
+    if (isset($_POST['parent_name']))   { $parentName   = $_POST['parent_name']; }   // Example : ANDRI SIREGAR/NANA SIREGAR
     if (isset($_POST['student_class'])) { $studentClass = $_POST['student_class']; } // Example : X.P-3
     if (isset($_POST['message']))       { $billList     = $_POST['message']; }     // Example : SPP AGU-2023=500,000; SPP SEP-2023=500,000; DAFTAR ULANG JUN-2024=2,315,000; PESAT FESTIVAL OKT-2024=100,000
+    If (!$parentName)   {error_function('Parent Name cannot NULL!',$waError);}
     If (!$studentClass) {error_function('Student Class cannot NULL!',$waError);}
     If (!$billList)     {error_function('Bill List cannot NULL!',$waError);}
+    $parentName = str_replace('Yth.', '', $parentName);
+    $parentName = str_replace('Yth ', '', $parentName);
+    $parentName = str_replace('Yth. ', '', $parentName);
 }
-
+if ($category === 'ppdb_regist') {
+    $noRegist = null;
+    if (isset($_POST['no_regist'])) { $noRegist = $_POST['no_regist']; }
+    If (!$noRegist) {error_function('No Regist cannot NULL!',$waError);}
+}
+if ($category === 'ppdb_accepted') {
+    $noRegist          = null;
+    $group             = null;
+    $bankAccountNumber = null;
+    $maxPayDate        = null;
+    if (isset($_POST['no_regist']))           { $noRegist = $_POST['no_regist']; }
+    if (isset($_POST['group']))               { $group = $_POST['group']; }
+    if (isset($_POST['bank_account_number'])) { $bankAccountNumber = $_POST['bank_account_number']; }
+    if (isset($_POST['max_pay_date']))        { $maxPayDate = $_POST['max_pay_date']; }
+    If (!$noRegist)          {error_function('No Regist cannot NULL!',$waError);}
+    If (!$group)             {error_function('Group cannot NULL!',$waError);}
+    If (!$bankAccountNumber) {error_function('Bank Account Number cannot NULL!',$waError);}
+    If (!$maxPayDate)        {error_function('Max Pay Date cannot NULL!',$waError);}
+}
+if ($category === 'ppdb_pay') {
+    $noRegist = null;
+    $nominal  = null;
+    if (isset($_POST['no_regist'])) { $noRegist = $_POST['no_regist']; }
+    if (isset($_POST['nominal']))   { $nominal = $_POST['nominal']; }
+    If (!$noRegist) {error_function('No Regist cannot NULL!',$waError);}
+    If (!$nominal)  {error_function('Nominal cannot NULL!',$waError);}
+}
 // Set path and payload for the request
 $postPath = '/qontak/chat/v1/broadcasts/whatsapp/direct';
 if ($category === 'absence') {
@@ -186,6 +226,58 @@ if ($category === 'bill') {
                 ["key" => "2", "value" => "student_name", "value_text" => $studentName],
                 ["key" => "3", "value" => "student_class", "value_text" => $studentClass],
                 ["key" => "4", "value" => "bill_list", "value_text" => $billList]
+            ]
+        ]
+    ];
+}
+if ($category === 'ppdb_regist') {
+    $postPayload = [
+        "to_name" => $studentName,
+        "to_number" => $phone, // "62812xxx" -> Must use international number 62,63,65, etc
+        "message_template_id" => "008eba6b-6acc-4cab-aec4-7c19a183c2d4",
+        "channel_integration_id" => "b9ac65e9-02cb-4ae8-bbbf-392d2801267f",
+        "language" => ["code" => "id"],
+        "parameters" => [
+            "body" => [
+                ["key" => "1", "value" => "school", "value_text" => $school],
+                ["key" => "2", "value" => "no_regist", "value_text" => $noRegist],
+                ["key" => "3", "value" => "student_name", "value_text" => $studentName]
+            ]
+        ]
+    ];
+}
+if ($category === 'ppdb_accepted') {
+    $postPayload = [
+        "to_name" => $studentName,
+        "to_number" => $phone, // "62812xxx" -> Must use international number 62,63,65, etc
+        "message_template_id" => "fd01cc07-d878-4582-a4f7-cf9b9d42e3a1",
+        "channel_integration_id" => "b9ac65e9-02cb-4ae8-bbbf-392d2801267f",
+        "language" => ["code" => "id"],
+        "parameters" => [
+            "body" => [
+                ["key" => "1", "value" => "no_regist", "value_text" => $noRegist],
+                ["key" => "2", "value" => "student_name", "value_text" => $studentName],
+                ["key" => "3", "value" => "group", "value_text" => $group],
+                ["key" => "4", "value" => "school", "value_text" => $school],
+                ["key" => "5", "value" => "school_year", "value_text" => $schoolYear],
+                ["key" => "6", "value" => "bank_account_number", "value_text" => $bankAccountNumber],
+                ["key" => "7", "value" => "max_pay_date", "value_text" => $maxPayDate]
+            ]
+        ]
+    ];
+}
+if ($category === 'ppdb_pay') {
+    $postPayload = [
+        "to_name" => $studentName,
+        "to_number" => $phone, // "62812xxx" -> Must use international number 62,63,65, etc
+        "message_template_id" => "004f79dd-ded2-4ddc-bad5-de564c3acc37",
+        "channel_integration_id" => "b9ac65e9-02cb-4ae8-bbbf-392d2801267f",
+        "language" => ["code" => "id"],
+        "parameters" => [
+            "body" => [
+                ["key" => "1", "value" => "no_regist", "value_text" => $noRegist],
+                ["key" => "2", "value" => "student_name", "value_text" => $studentName],
+                ["key" => "3", "value" => "nominal", "value_text" => $nominal]
             ]
         ]
     ];
